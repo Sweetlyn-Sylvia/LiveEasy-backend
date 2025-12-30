6a4e67c345e025c3d19a48716580f3bf0b15bc94
Sweetlyn-Sylvia <sweetlynsylvia@gmail.com>
2025-12-30 11:30:19 +0530

    initial

--- Program.cs
+++ Program.cs
@@ -1,26 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using ParcelTrackingSystem.Data;
 
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddHttpClient();
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure CORS to allow your Angular frontend
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAngularApp",
-        policy => policy.WithOrigins("http://localhost:4200")
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
                         .AllowAnyMethod()
                         .AllowAnyHeader());
 });
-
 
 var app = builder.Build();
 
@@ -32,9 +30,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseAuthorization();
-app.UseCors("AllowAngularApp");
 
+// Enable CORS (must match the policy name)
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
